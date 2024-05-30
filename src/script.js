a55384ca0b8651121f6700f6a5b08989d7c0d7f4
Tilo Jäkel <tilo.jaekel@iem.thm.de>
2024-05-30 14:00:29 +0200

    Replace index.html

--- src/script.js
+++ src/script.js
@@ -1,1 +1,56 @@
-ECHO ist eingeschaltet (ON).
+<!DOCTYPE html>
+            <html lang="de">
+            <head>
+                <meta charset="UTF-8">
+                <title>THM Universum</title>
+                <link rel="stylesheet" href="styles.css">
+            </head>
+            <body>
+            <header>
+                <img src="assets/Bilder/THM_Logo_4c.png" />
+                <h1>Universum</h1>
+            </header>
+            <main>
+                <div class="container" id="wpr">
+                    <h2>Webprogrammierung</h2>
+                    <div class="planet">
+                        <div id="vignette"></div>
+                    </div>
+                    <div class="moon">
+                    </div>
+                </div>
+                <div class="container" id="ma">
+                    <h2>Mobile Anwendungen</h2>
+                    <div class="planet">
+                        <div id="vignette"></div>
+                    </div>
+                    <div class="moon">
+                    </div>
+                </div>
+                <div class="container" id="gd">
+            <h2>Grafische Datenverarbeitung</h2>
+            <div class="planet">
+                <div id="vignette"></div>
+            </div>
+            <div class="moon">
+            </div>
+        </div>
+        <div class="container" id="av">
+            <h2>Audiovisuelle Medien</h2>
+            <div class="planet">
+                <div id="vignette"></div>
+            </div>
+            <div class="moon">
+            </div>
+        </div>
+    </main>
+    <footer>
+        <p id="copyright">©THM</p>
+    </footer>
+    <p id="textbox"></p>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/MotionPathPlugin.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/TextPlugin.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
