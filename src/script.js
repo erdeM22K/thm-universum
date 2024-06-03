d0dea2ee7971da38c56f64d3750992e68ab53515
Dennis Bezusov <dennis.bezusov@iem.thm.de>
2024-06-03 20:28:37 +0200

    Replace script.js

--- src/script.js
+++ src/script.js
@@ -1,56 +1,123 @@
-<!DOCTYPE html>
-            <html lang="de">
-            <head>
-                <meta charset="UTF-8">
-                <title>THM Universum</title>
-                <link rel="stylesheet" href="styles.css">
-            </head>
-            <body>
-            <header>
-                <img src="assets/Bilder/THM_Logo_4c.png" />
-                <h1>Universum</h1>
-            </header>
-            <main>
-                <div class="container" id="wpr">
-                    <h2>Webprogrammierung</h2>
-                    <div class="planet">
-                        <div id="vignette"></div>
-                    </div>
-                    <div class="moon">
-                    </div>
-                </div>
-                <div class="container" id="ma">
-                    <h2>Mobile Anwendungen</h2>
-                    <div class="planet">
-                        <div id="vignette"></div>
-                    </div>
-                    <div class="moon">
-                    </div>
-                </div>
-                <div class="container" id="gd">
-            <h2>Grafische Datenverarbeitung</h2>
-            <div class="planet">
-                <div id="vignette"></div>
-            </div>
-            <div class="moon">
-            </div>
-        </div>
-        <div class="container" id="av">
-            <h2>Audiovisuelle Medien</h2>
-            <div class="planet">
-                <div id="vignette"></div>
-            </div>
-            <div class="moon">
-            </div>
-        </div>
-    </main>
-    <footer>
-        <p id="copyright">©THM</p>
-    </footer>
-    <p id="textbox"></p>
-    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/MotionPathPlugin.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/TextPlugin.min.js"></script>
-    <script src="script.js"></script>
-</body>
-</html>
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const feed = document.getElementById('planeten_container');
+
+
+
+    // Funktion, um die Inhalte am Ende zu duplizieren
+    function duplicateContent() {
+        const containers = feed.querySelectorAll('.planet_container');
+        containers.forEach(container => {
+            const clone = container.cloneNode(true);
+            feed.appendChild(clone);
+        });
+    }
+
+    // Funktion zur Initialisierung des Skripts
+    function init() {
+        // Initiale Duplizierung der Inhalte
+        duplicateContent();
+
+        feed.addEventListener('scroll', () => {
+            // Wenn der Benutzer sich dem Ende nähert, weitere Inhalte hinzufügen
+            if (feed.scrollTop + feed.clientHeight >= feed.scrollHeight - 50) {
+                duplicateContent();
+            }
+        });
+    }
+
+
+
+    function starAnimationDesktop () {
+        gsap.from('.planet', {
+            y: -1500,
+            duration: 6,
+            stagger: 2,
+            ease: 'power4.out',
+            delay: 3
+        });
+        gsap.from('.planet_title', {
+            y: -1500,
+            duration: 6,
+            stagger: 2,
+            ease: 'power4.out',
+            delay: 3
+        });
+        gsap.from('#title', {
+            y: -1500,
+            duration: 4,
+            ease: 'power4.out',
+            delay: 1
+        });
+        gsap.from('#logo', {
+            x: 1500,
+            duration: 3,
+            ease: 'power4.out'
+        });
+    }
+    function starAnimationMobile () {
+        gsap.from('.planet', {
+            x: -1500,
+            duration: 3,
+            stagger: 0.5,
+            ease: 'power4.out',
+            delay: 4
+        });
+        gsap.from('.planet_title', {
+            x: -1500,
+            duration: 3,
+            stagger: 0.5,
+            ease: 'power4.out',
+            delay: 4
+        });
+        gsap.from('#title', {
+            y: -2500,
+            duration: 4,
+            ease: 'power4.out',
+            delay: 1
+        });
+        gsap.from('#logo', {
+            x: 500,
+            duration: 3,
+            ease: 'power4.out'
+        });
+    }
+
+    // Medienabfrage für Bildschirmbreiten ab 768px
+    const mediaQuery = window.matchMedia('(max-width: 480px)');
+
+    // Eventlistener, um die Funktion zu initialisieren, wenn die Medienabfrage erfüllt ist
+    function handleMediaQueryChange(event) {
+        if (event.matches) {
+            init();
+            starAnimationMobile ()
+            
+        }
+    }
+
+    // Initialer Check und Hinzufügen des Eventlisteners
+    if (mediaQuery.matches) {
+        init();
+        starAnimationMobile ()
+    }
+    mediaQuery.addListener(handleMediaQueryChange);
+
+    const mediaQuery2 = window.matchMedia('(min-width: 481px)');
+
+    // Eventlistener, um die Funktion zu initialisieren, wenn die Medienabfrage erfüllt ist
+    function handleMediaQueryChange2(event) {
+        if (event.matches) {
+        starAnimationDesktop ()
+            
+        }
+    }
+
+    // Initialer Check und Hinzufügen des Eventlisteners
+    if (mediaQuery2.matches) {
+
+        starAnimationDesktop ()
+    }
+    mediaQuery2.addListener(handleMediaQueryChange2);
+});
+
